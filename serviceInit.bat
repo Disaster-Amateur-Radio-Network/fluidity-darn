5d4d5e87595a4fad110fd5824ada63a4124215f0
Sean Lynch <sean@constantdigital.com>
2023-03-21 14:08:49 -0700

    automate nssm setup

---
+++ serviceInit.bat
@@ -0,0 +1,32 @@
+md C:\Users\admin\fluidity-logs
+
+cd C:\Users\admin\nssm-2.24\win64
+nssm install FluidityAgentService
+
+nssm set FluidityAgentService Application C:\Users\admin\fluidity-darn\agent.bat
+nssm set FluidityAgentService AppDirectory C:\Users\admin\fluidity-darn
+
+nssm set FluidityAgentService DisplayName FluidityAgent
+nssm set FluidityAgentService Description Fluidity Agent Service
+nssm set FluidityAgentService Start SERVICE_AUTO_START
+
+nssm set FluidityAgentService ObjectName LocalSystem
+
+nssm set FluidityAgentService AppStopMethodSkip 0
+nssm set FluidityAgentService AppStopMethodConsole 1500
+nssm set FluidityAgentService AppStopMethodWindow 1500
+nssm set FluidityAgentService AppStopMethodThreads 1500
+
+nssm set FluidityAgentService AppThrottle 10000
+nssm set FluidityAgentService AppExit Default Restart
+nssm set FluidityAgentService AppRestartDelay 60000
+
+nssm set FluidityAgentService AppStdout C:\Users\admin\fluidity-logs\out.log
+nssm set FluidityAgentService AppStderr C:\Users\admin\fluidity-logs\error.log
+
+nssm set FluidityAgentService AppStdoutCreationDisposition 4
+nssm set FluidityAgentService AppStderrCreationDisposition 4
+nssm set FluidityAgentService AppRotateFiles 1
+nssm set FluidityAgentService AppRotateOnline 0
+nssm set FluidityAgentService AppRotateSeconds 86400
+nssm set FluidityAgentService AppRotateBytes 1048576
