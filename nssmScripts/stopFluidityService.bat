0210c307aac7f00cc8f652b334aa650079bc7fcf
Sean Lynch <sean@constantdigital.com>
2023-03-21 14:41:25 -0700

    add debug info to bat files

--- nssmScripts/stopFluidityService.bat
+++ nssmScripts/stopFluidityService.bat
@@ -2,6 +2,8 @@ SET NSSMDIR=C:\Users\admin\nssm-2.24\win64
 SET "CURDIR=%cd%"
 cd %NSSMDIR%
 
+nssm status FluidityAgentService
 nssm stop FluidityAgentService
+nssm status FluidityAgentService
 
 cd %CURDIR%
